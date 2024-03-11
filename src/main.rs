a213f5b28208cf6977cad55de82f1276874f4a67
emrekizilelma <emrekizilelma00@gmail.com>
2024-03-11 16:23:00 +0300

    io

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use calculrs;
-
+use std::io;
 fn main() {
+    
     calculrs::calculrs_menu();
+
+    let mut op = String::new();
+
+    println!("Enter your option: ");
+
+    io::stdin()
+        .read_line(&mut op)
+        .expect("Failed to read line");
+
+    
 }
